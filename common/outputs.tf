e0c970b7770735841432a6bd97697a6179598447
Brandon Sorenson <sorebran@isu.edu>
2023-05-14 16:15:35 -0600

    fix liatrio ip output reference

--- common/outputs.tf
+++ common/outputs.tf
@@ -4,10 +4,10 @@ output "client_certificate" {
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.liatrio.kube_config_raw
+  value     = azurerm_kubernetes_cluster.liatrio.kube_config_raw
   sensitive = true
 }
 
-output  "liatrio_ip" {
-  value = azurerm_public_ip.liatrio
+output "liatrio_ip" {
+  value = data.azurerm_public_ip.liatrio
 }
