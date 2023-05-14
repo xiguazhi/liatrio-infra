14dcf44f07cab2b37fa09f8fdb5cbbfbfbfc4d0c
Brandon Sorenson <sorebran@isu.edu>
2023-05-14 16:11:40 -0600

    fix container registry to comly with azure naming conventions

--- common/azure-aci.tf
+++ common/azure-aci.tf
@@ -1,12 +1,7 @@
-resource "azurerm_container_registry" "acr" {
-  name                = "Liatrio-Demo"
+resource "azurerm_container_registry" "liatrio" {
+  name                = "liatrioacr"
   resource_group_name = azurerm_resource_group.liatrio.name
   location            = azurerm_resource_group.liatrio.location
   sku                 = "Standard"
   admin_enabled       = false
-
-  encryption {
-    enabled = true
-    key_vault_key_id = azurerm_key_vault.liatrio.id
-  }
 }
