df0ca25e33d4e50a162aedc57cab79fca178abeb
Brandon Sorenson <sorebran@isu.edu>
2023-05-14 16:21:51 -0600

    modify subnets variable change keyvault and resource group names

--- common/variables.tf
+++ common/variables.tf
@@ -1,19 +1,26 @@
 variable "resource_prefix" {}
-variable "rgrp_name" {}
-variable "kv_name" {}
+variable "resource_group_name" {}
+variable "key_vault_name" {}
 variable "subnets" {
+  type = list(object({
+    name = string
+    prefix = string
+    services = list(string)
+    service_delegation = bool
+
+  }))
   default = [
     {
-      name = "PLACEHOLDER1"
-      prefix = "10.0.0.0/24"
-      services = []
-      service_delegaation = false
+      name                = "PLACEHOLDER1"
+      prefix              = "10.0.0.0/24"
+      services            = []
+      service_delegation = false
     },
     {
-      name = "PLACEHOLDER2"
-      prefix = "10.2.10.0/24"
-      services = []
-      service_delegaation = true
+      name                = "PLACEHOLDER2"
+      prefix              = "10.2.10.0/24"
+      services            = []
+      service_delegation = true
 
     }
   ]
