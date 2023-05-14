7580ad084143780fa0b711775b6e1ace2b9210a1
Brandon Sorenson <sorebran@isu.edu>
2023-05-14 12:36:57 -0600

    define outputs for infra

---
+++ common/outputs.tf
@@ -0,0 +1,13 @@
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.liatrio.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.liatrio.kube_config_raw
+  sensitive = true
+}
+
+output  "liatrio_ip" {
+  value = azurerm_public_ip.liatrio
+}
