5d75d7fdda55c25e53f4284fd9d0d38610008b54
Brandon Sorenson <sorebran@isu.edu>
2023-05-15 08:55:20 -0600

    remove keyvault, cant use for encrypting acr unless we upgrade

--- common/azurerm-kv.tf
+++ common/azurerm-kv.tf
@@ -13,51 +13,6 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault" "liatrio" {
-  name                        = format("%s-%s-kv", var.resource_prefix, var.key_vault_name)
-  location                    = azurerm_resource_group.liatrio.location
-  resource_group_name         = azurerm_resource_group.liatrio.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-
-  network_acls {
-    bypass                     = "AzureServices"
-    default_action             = "Deny"
-    ip_rules                   = ["199.192.99.26"]
-    virtual_network_subnet_ids = concat([for sub in var.subnets : azurerm_subnet.sub[sub.name].id])
-  }
-}
-
-resource "azurerm_key_vault_access_policy" "SP-poliy" {
-  key_vault_id = azurerm_key_vault.liatrio.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-}
-
-resource "azurerm_key_vault_key" "encryption" {
-  name         = "acr-key"
-  key_vault_id = azurerm_key_vault.liatrio.id
-  key_type     = "RSA"
-  key_size     = 4096
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
-  rotation_policy {
-    automatic {
-      time_before_expiry = "P30D"
-    }
-    expire_after         = "P90D"
-    notify_before_expiry = "P29D"
-  }
-}
-
 
 #resource "azurerm_key_vault_access_policy" "k8s-policy" {
 #    key_vault_id = azurerm_key_vault.liatrio.id
